04ab3ee3176030cef0fe1641ed9589630c7101e6
Yokomi_ <skyingfloor@gmail.com>
2023-08-02 08:54:56 +0700

    Hopper-Prefill-Mod-1.19.4

---
+++ src/main/java/name/hopper_prefill_mod/HopperPrefillModClient.java
@@ -0,0 +1,10 @@
+package name.hopper_prefill_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HopperPrefillModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
